db6ed6df96293941ddf9eac0d2527842f3e8434e
Tomcountry <80274069+Tomcountry@users.noreply.github.com>
2021-03-15 17:04:05 +0700

    Update test-api.py
    add 1

--- test-api.py
+++ test-api.py
@@ -1,6 +1,8 @@
 import ccxt
 import pandas as pd
 import time
+import json
+import csv
 import os
 from os import environ
 import requests
@@ -12,12 +14,23 @@ apiKey = "eqg-7JV0"
 secret = "lPj4ExgDKshIZE_7BlKAt-GDbKeZyZVPQQ--xzG00xg"
 exchange = ccxt.deribit({'apiKey': apiKey ,'secret': secret,'enableRateLimit': True,"urls": {"api": "https://test.deribit.com"}})
 
+pair = "BTC-PERPETUAL"
+size_order = 10 # USD
+types = 'limit'
+side = 'buy'
+price = 1000
+postOnly =  False
+reduceOnly = False
+ioc = False
+timestampUntil = 1615184598249
 
-dfMatchOrder1 = pd.DataFrame(exchange.fetchMyTrades("BTC-PERPETUAL",limit=3),
-                                 columns=['id', 'datetime', 'symbol', 'side', 'price', 'amount', "fee"])
+dfZone = pd.read_csv("ZoneTestCSV.csv")
+dfTransaction = pd.read_csv("TransactionTestCSV.csv")
 
-while True:
-    print(joker)
-    print(batman)
-    print(dfMatchOrder1)
-    time.sleep(5)
+#ราคาตลาด (ต้องแก้ไข)
+r1 = json.dumps(exchange.fetch_ticker('BTC-PERPETUAL'))
+dataPriceBTC = json.loads(r1)
+MarketPrice = dataPriceBTC['last']
+print('BTC-PERPETUAL=',MarketPrice,'$')
+
+
