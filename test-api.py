3435d06252573cb8b637462d76feb8e2eda449c0
Tomcountry <p.sinsukh@gmail.com>
2021-03-15 16:34:51 +0700

    unhide

--- test-api.py
+++ test-api.py
@@ -6,11 +6,11 @@ from os import environ
 import requests
 
 
-joker = environ["api"]
-batman = environ["secret"]
-# apiKey = "eqg-7JV0"
-# secret = "lPj4ExgDKshIZE_7BlKAt-GDbKeZyZVPQQ--xzG00xg"
-exchange = ccxt.deribit({'apiKey': joker ,'secret': batman,'enableRateLimit': True,"urls": {"api": "https://test.deribit.com"}})
+# joker = environ["api"]
+# batman = environ["secret"]
+apiKey = "eqg-7JV0"
+secret = "lPj4ExgDKshIZE_7BlKAt-GDbKeZyZVPQQ--xzG00xg"
+exchange = ccxt.deribit({'apiKey': apiKey ,'secret': secret,'enableRateLimit': True,"urls": {"api": "https://test.deribit.com"}})
 
 
 dfMatchOrder1 = pd.DataFrame(exchange.fetchMyTrades("BTC-PERPETUAL",limit=3),
