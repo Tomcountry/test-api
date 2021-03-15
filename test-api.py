8fa6a521619456aa8f868da563960df0c4e9e5dd
Tomcountry <p.sinsukh@gmail.com>
2021-03-15 15:29:04 +0700

    environ

--- test-api.py
+++ test-api.py
@@ -1,9 +1,12 @@
 import ccxt
 import pandas as pd
 import time
+import os
+from os import environ
 
-apiKey = "eqg-7JV0"
-secret = "lPj4ExgDKshIZE_7BlKAt-GDbKeZyZVPQQ--xzG00xg"
+
+apiKey = environ["api"]
+secret = environ["secret"]
 exchange = ccxt.deribit({'apiKey': apiKey ,'secret': secret,'enableRateLimit': True,"urls": {"api": "https://test.deribit.com"}})
 
 
@@ -11,5 +14,7 @@ dfMatchOrder1 = pd.DataFrame(exchange.fetchMyTrades("BTC-PERPETUAL",limit=3),
                                  columns=['id', 'datetime', 'symbol', 'side', 'price', 'amount', "fee"])
 
 while True:
+    print(apiKey)
+    print(secret)
     print(dfMatchOrder1)
     time.sleep(5)
