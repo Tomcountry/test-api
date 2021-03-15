40bd97ae971241455435371684ae03fc01619df0
Tomcountry <p.sinsukh@gmail.com>
2021-03-15 15:39:53 +0700

    joker

--- test-api.py
+++ test-api.py
@@ -2,11 +2,11 @@ import ccxt
 import pandas as pd
 import time
 import os
+from os import environ
 
-
-
-apiKey ="dasdasd"
-secret = "dsadsadd"
+joker = environ["api"]
+apiKey = "eqg-7JV0"
+secret = "lPj4ExgDKshIZE_7BlKAt-GDbKeZyZVPQQ--xzG00xg"
 exchange = ccxt.deribit({'apiKey': apiKey ,'secret': secret,'enableRateLimit': True,"urls": {"api": "https://test.deribit.com"}})
 
 
@@ -14,5 +14,6 @@ dfMatchOrder1 = pd.DataFrame(exchange.fetchMyTrades("BTC-PERPETUAL",limit=3),
                                  columns=['id', 'datetime', 'symbol', 'side', 'price', 'amount', "fee"])
 
 while True:
+    print(joker)
     print(dfMatchOrder1)
     time.sleep(5)
